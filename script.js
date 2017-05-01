ee336bd1e9bff1da342519dae5e79fe4027ecead
zulloi <zulloi220@gmail.com>
2017-05-01 10:07:10 -0400

    images & formatting 😍 😍

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 ghosthead = false;
 window.addEventListener('scroll', function(e) {
-  console.log(ghosthead);
-  if (ghosthead == false && window.scrollY >= document.documentElement.clientHeight ) {
-    ghosthead = true;
-    document.getElementsByTagName('header')[0].className = 'ghostly';
-    console.log(document.getElementsByTagName('header'));
-  }
-  else if (ghosthead == true && window.scrollY <= document.documentElement.clientHeight){
-    ghosthead = false;
-    document.getElementsByTagName('header')[0].className = 'thicc';
-    console.log(document.getElementsByTagName('header'));
-  }
+    console.log(ghosthead);
+    if (ghosthead == false && window.scrollY >= document.documentElement.clientHeight) {
+        ghosthead = true;
+        document.getElementsByTagName('header')[0].className = 'scroll';
+        console.log(document.getElementsByTagName('header'));
+    } else if (ghosthead == true && window.scrollY <= document.documentElement.clientHeight) {
+        ghosthead = false;
+        document.getElementsByTagName('header')[0].className = 'top';
+        console.log(document.getElementsByTagName('header'));
+    }
 });
